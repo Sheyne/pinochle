fee6fdd3b8879cf2d4214b213519beb5eea56dc8
Sheyne Anderson <sheyne@sheyne.com>
2020-03-11 06:12:36 +0000

    Use relative paths

--- client/main.js
+++ client/main.js
@@ -1,6 +1,6 @@
 import init, { run_app } from './pkg/pinochle_client.js';
 async function main() {
-   await init('/pkg/pinochle_client_bg.wasm');
+   await init('pkg/pinochle_client_bg.wasm');
    run_app();
 }
 main()
